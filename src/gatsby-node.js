8aab83af82835d1bdfedd0ac2520a2790af57891
Kieran White <kieran@bluesquareoffices.com>
2020-03-14 20:00:56 +0000

    Adding follow-redirects so fetch can get the redirected page rather than initial.

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,3 +1,4 @@
+import { https } from 'follow-redirects';
 import fetch from 'node-fetch';
 import cheerio from 'cheerio';
 import { v4 as uuidv4 } from 'uuid';
@@ -18,47 +19,49 @@ exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }, { p
     throw new Error("You must supply a valid place id from Google. You can find your place id at https://developers.google.com/maps/documentation/javascript/examples/places-placeid-finder.");
   }
 
-  const url = `https://search.google.com/local/reviews?placeid=${placeId}`;
-  
-  return fetch(url)
-  .then(res => res.text())
-  .then(body => {
-    const $ = cheerio.load(body);
-    let reviews = [];
+  https.get(`https://search.google.com/local/reviews?placeid=${placeId}`, response => {
+    return fetch(response.responseUrl)
+      .then(res => res.text())
+      .then(body => {
+        const $ = cheerio.load(body);
+        let reviews = [];
 
-    $('.gws-localreviews__general-reviews-block .gws-localreviews__google-review').each(function () {
-      let review = {
-        id: '',
-        author: '',
-        content: '',
-        score: '',
-        createdAt: '',
-      };
-      review.id = uuidv4();
-      review.author = $(this).find('img').attr('alt');
-      review.content = stripHtml($(this).find('.review-full-text').text());
-      review.score = $(this).find('g-review-stars > span').attr('aria-label').replace('Rated ', '').replace('.0 out of 5,', '');
-      review.createdAt = formatDate($(this).find('g-review-stars + span').text());
+        $('.gws-localreviews__general-reviews-block .gws-localreviews__google-review').each(function () {
+          let review = {
+            id: '',
+            author: '',
+            content: '',
+            score: '',
+            createdAt: '',
+          };
+          review.id = uuidv4();
+          review.author = $(this).find('img').attr('alt');
+          review.content = stripHtml($(this).find('.review-full-text').text());
+          review.score = $(this).find('g-review-stars > span').attr('aria-label').replace('Rated ', '').replace('.0 out of 5,', '');
+          review.createdAt = formatDate($(this).find('g-review-stars + span').text());
 
-      reviews.push(review);
-    });
+          reviews.push(review);
+        });
 
-    reviews.forEach(review => {
-      const nodeContent = JSON.stringify(review);
-      const nodeMeta = {
-        id: createNodeId(`google-review-${review.id}`),
-        parent: null,
-        children: [],
-        internal: {
-          type: `GoogleReview`,
-          content: nodeContent,
-          contentDigest: createContentDigest(review)
-        }
-      };
-      const node = Object.assign({}, review, nodeMeta);
-      createNode(node);
-    });
+        reviews.forEach(review => {
+          const nodeContent = JSON.stringify(review);
+          const nodeMeta = {
+            id: createNodeId(`google-review-${review.id}`),
+            parent: null,
+            children: [],
+            internal: {
+              type: `GoogleReview`,
+              content: nodeContent,
+              contentDigest: createContentDigest(review)
+            }
+          };
+          const node = Object.assign({}, review, nodeMeta);
+          createNode(node);
+        });
 
-    return;
+        return;
+      });
+  }).on('error', err => {
+    throw new Error("Error accessing the Google page. Try again later..")
   });
 };
