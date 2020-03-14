6547efc0d7f2a7a7963ed7dfd0d9d7941057db11
Kieran White <kieran@bluesquareoffices.com>
2020-03-14 20:39:00 +0000

    Using ScaleSerp API for fetching data as more reliable than cheerio scraping.

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,67 +1,45 @@
-import { https } from 'follow-redirects';
-import fetch from 'node-fetch';
-import cheerio from 'cheerio';
-import { v4 as uuidv4 } from 'uuid';
-import stripHtml from 'string-strip-html';
-import moment from 'moment';
 
-const now = moment();
+const axios = require('axios');
 
-const formatDate = (dateString) => {
-  var dateStringArray = dateString.split(' ');
-  return now().subtract(dateStringArray[0], dateStringArray[1]).toISOString();
-}
-
-exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }, { placeId }) => {
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }, { placeId, apiKey }) => {
   const { createNode } = actions;
+
+  if (!apiKey || typeof apiKey !== 'string') {
+    throw new Error("You must supply a valid API Key from Scale Serp. Visit https://scaleserp.com/ for more information.");
+  }
 
   if (!placeId || typeof placeId !== 'string') {
     throw new Error("You must supply a valid place id from Google. You can find your place id at https://developers.google.com/maps/documentation/javascript/examples/places-placeid-finder.");
   }
 
-  https.get(`https://search.google.com/local/reviews?placeid=${placeId}`, response => {
-    return fetch(response.responseUrl)
-      .then(res => res.text())
-      .then(body => {
-        const $ = cheerio.load(body);
-        let reviews = [];
+  const params = {
+    api_key: apiKey,
+    search_type: "place_reviews",
+    place_id: placeId,
+  };
 
-        $('.gws-localreviews__general-reviews-block .gws-localreviews__google-review').each(function () {
-          let review = {
-            id: '',
-            author: '',
-            content: '',
-            score: '',
-            createdAt: '',
-          };
-          review.id = uuidv4();
-          review.author = $(this).find('img').attr('alt');
-          review.content = stripHtml($(this).find('.review-full-text').text());
-          review.score = $(this).find('g-review-stars > span').attr('aria-label').replace('Rated ', '').replace('.0 out of 5,', '');
-          review.createdAt = formatDate($(this).find('g-review-stars + span').text());
+  axios.get('https://api.scaleserp.com/search', { params })
+  .then(response => {
+    const reviews = response.place_reviews_results;
 
-          reviews.push(review);
-        });
+    reviews.forEach(review => {
+      const nodeContent = JSON.stringify(review);
+      const nodeMeta = {
+        id: createNodeId(`google-review-${review.id}`),
+        parent: null,
+        children: [],
+        internal: {
+          type: `GoogleReview`,
+          content: nodeContent,
+          contentDigest: createContentDigest(review)
+        }
+      };
+      const node = Object.assign({}, review, nodeMeta);
+      createNode(node);
+    });
 
-        reviews.forEach(review => {
-          const nodeContent = JSON.stringify(review);
-          const nodeMeta = {
-            id: createNodeId(`google-review-${review.id}`),
-            parent: null,
-            children: [],
-            internal: {
-              type: `GoogleReview`,
-              content: nodeContent,
-              contentDigest: createContentDigest(review)
-            }
-          };
-          const node = Object.assign({}, review, nodeMeta);
-          createNode(node);
-        });
-
-        return;
-      });
-  }).on('error', err => {
-    throw new Error("Error accessing the Google page. Try again later..")
+    return;
+  }).catch(error => {
+    throw new Error(`Error fetching results from ScaleSerp API: ${error}`);
   });
 };
